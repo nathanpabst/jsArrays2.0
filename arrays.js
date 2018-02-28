83d540b87711fb66a246b0bf2e5983811550f7ff
Nathan Pabst <nathan.pabst@gmail.com>
2018-02-27 21:33:27 -0600

    completed challenge 2

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ var test4 = "mom"; //true
  
 var test1 = "cat";
 var opp = test1.split("").reverse().join("");
-if (test === opp){
+if (test1 === opp){
     console.log("same? ", true);
 } else{
     console.log("same? ", false);
@@ -18,15 +18,11 @@ if (test === opp){
 
 var animals = ["fish", "dog", "cat"];
 var animalzElement = document.getElementById('animalz');
+console.log("animals", document.getElementById('animalz'))
 for(var i=0; i<animals.length; i++){
-    // console.log("animal", animals[i]) //console
-animalzElement.innerHTML += animals[i]; // DOM
+    // console.log("animal", animals[i]) 
+    animalzElement.innerHTML += animals[i]; 
 }
-
-
-// var book3 = "Fahrenheit 451";
-// var removeLetters = book3.replace(/\D/,'');
-// console.log(removeLetters);
 
 //challenge 2
 
@@ -34,7 +30,7 @@ var book = "catch 22"
 var bookSplit = book.split("");
 var realNumz = [];
 for(var j =0; j<bookSplit.length; j++){
-    if((bookSplit[j]*1){
+    if(bookSplit[j]*1){
         realNumz.push(bookSplit[j]);
     }
 }
